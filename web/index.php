9c6b28363d56b846e8cb8fa7ef92f3606f78297c
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2020-12-30 16:50:01 +0530

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,101 +1,63 @@
 <?php
 require_once __DIR__ . "/config.php";
-use kyle2142\PHPBot;
 
-// Set the bot TOKEN
-$bot_id = $GLOBALS["TG_BOT_TOKEN"];
-$bot = new PHPBot($bot_id);
-$content = file_get_contents("php://input");
-$update = json_decode($content, true);
+use skrtdev\NovaGram\Bot;
+use skrtdev\Telegram\{Message, Exception as TelegramException};
 
-if (isset($update["message"])) {
-    $message_id = $update["message"]["message_id"];
-    $chat_id = $update["message"]["chat"]["id"];
+$Bot = new Bot($GLOBALS["TG_BOT_TOKEN"], [
+    "disable_ip_check" => true,
+    "parse_mode" => "HTML",
+    "disable_notification" => true,
+    "disable_web_page_preview" => true,
+    "debug" => $GLOBALS["TG_DUMP_CHANNEL_ID"],
+]);
 
-    if (isset($update["message"]["text"])) {
-        $message_text = $update["message"]["text"];
-        if (strpos($message_text, "/start ") !== FALSE) {
-            $message_params = explode(" ", $message_text);
-            if (strpos($message_params[1], "_") !== FALSE) {
-                $msg_param_s = explode("_", $message_params[1]);
-                $req_message_id = $msg_param_s[1];
-                try {
-                    $bot->api->forwardMessage(array(
-                        "chat_id" => $chat_id,
-                        "from_chat_id" => $GLOBALS["TG_DUMP_CHANNEL_ID"],
-                        "disable_notification" => True,
-                        "message_id" => $req_message_id
-                    ));
-                }
-                catch (Exception $e) {
-                    /**
-                     * sometimes, forwarding FAILS 😉
-                     */
-                }
+$Bot->onCommand('start', function(Message $message, array $args) use ($Bot) {
+    $chat = $message->chat;
+
+    if(empty($args) || $args[0] === ""){
+        $message->reply($GLOBALS["START_MESSAGE"]);
+    }
+    else{
+        if (strpos($args[0], "_") !== FALSE) {
+            $msg_param_s = explode("_", $args[0]);
+            $req_message_id = $msg_param_s[1];
+            try {
+                $chat->copyMessage([
+                    "from_chat_id" => $GLOBALS["TG_DUMP_CHANNEL_ID"],
+                    "message_id" => $req_message_id
+                ]);
             }
-            else {
-                $bot->api->deleteMessage(array(
-                    "chat_id" => $chat_id,
-                    "message_id" => $message_id
-                ));
+            catch (TelegramException $e) {
+                /**
+                 * sometimes, forwarding FAILS 😉
+                 */
             }
-        }
-        else if (strpos($message_text, "/start") !== FALSE) {
-            $bot->api->sendMessage(array(
-                "chat_id" => $chat_id,
-                "text" => $GLOBALS["START_MESSAGE"],
-                "parse_mode" => "HTML",
-                "disable_notification" => True,
-                "reply_to_message_id" => $message_id
-            ));
         }
         else {
-            $bot->api->deleteMessage(array(
-                "chat_id" => $chat_id,
-                "message_id" => $message_id
-            ));
+            $message->delete();
         }
     }
-    else {
+});
+
+$Bot->onMessage(function (Message $message){
+    if(!isset($message->text)){
         if ($GLOBALS["IS_PUBLIC"] !== FALSE) {
-            get_link($bot, $chat_id, $message_id);
+            $message->getLink();
         }
         else if (in_array($chat_id, $GLOBALS["TG_AUTH_USERS"])) {
-            get_link($bot, $chat_id, $message_id);
+            $message->getLink();
         }
         else {
-            $bot->api->deleteMessage(array(
-                "chat_id" => $chat_id,
-                "message_id" => $message_id
-            ));
+            $message->delete();
         }
     }
-}
+});
 
 
-function get_link($bot, $chat_id, $message_id) {
-    $status_message = $bot->api->sendMessage(array(
-        "chat_id" => $chat_id,
-        "text" => $GLOBALS["CHECKING_MESSAGE"],
-        "parse_mode" => "HTML",
-        "disable_web_page_preview" => True,
-        "disable_notification" => True,
-        "reply_to_message_id" => $message_id
-    ));
-
-    $req_message = $bot->api->forwardMessage(array(
-        "chat_id" => $GLOBALS["TG_DUMP_CHANNEL_ID"],
-        "from_chat_id" => $chat_id,
-        "disable_notification" => True,
-        "message_id" => $message_id
-    ));
-
-    $required_url = "https://t.me/" . $GLOBALS["TG_BOT_USERNAME"] . "?start=" . "view" . "_" . $req_message->message_id . "_" . "tg";
-
-    $bot->api->editMessageText(array(
-        "chat_id" => $chat_id,
-        "message_id" => $status_message->message_id,
-        "text" => $required_url,
-        "disable_web_page_preview" => True
-    ));
-}
+Message::addMethod("getLink", function (){
+    $status_message = $this->reply($GLOBALS["CHECKING_MESSAGE"]);
+    $req_message = $this->forward($GLOBALS["TG_DUMP_CHANNEL_ID"]);
+    $required_url = "https://t.me/" . $GLOBALS["TG_BOT_USERNAME"] . "?start=" . "view_{$req_message->message_id}_tg";
+    $status_message->editText($required_url);
+});
